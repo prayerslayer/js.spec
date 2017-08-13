46d87e570b1aaac24f646f3ee9e0518977066fd8
Nikolaus Piccolotto <prayerslayer@gmail.com>
2017-08-13 19:51:48 +0200

    Warn about undefined predicates in specs

--- test/util.test.js
+++ test/util.test.js
@@ -1,11 +1,35 @@
-import { expect } from 'chai'
-import { zip } from '../lib/util'
+import { expect } from "chai";
+import { zip, undefinedPredicateWarning } from "../lib/util";
 
 describe("util", () => {
   describe("zip", () => {
     it("is reversible", () => {
-      const colls = [[1, 2, 3], [4, 5, 6]]
-      expect(colls).to.deep.equal(zip(...zip(...colls)))
-    })
-  })
-})
+      const colls = [[1, 2, 3], [4, 5, 6]];
+      expect(colls).to.deep.equal(zip(...zip(...colls)));
+    });
+  });
+
+  describe("undefinedPredicateWarning", () => {
+    const originalConsoleWarn = console.warn;
+
+    afterEach(() => {
+      console.warn = originalConsoleWarn;
+    });
+    it("prints with arrays", done => {
+      console.warn = () => done();
+      undefinedPredicateWarning("Array", [null]);
+    });
+    it("prints with objects", done => {
+      console.warn = () => done();
+      undefinedPredicateWarning("Array", { foo: undefined });
+    });
+    it("is silent with other types", done => {
+      console.warn = done;
+      undefinedPredicateWarning("String", { foo: "undefined" });
+      undefinedPredicateWarning("Bool", { foo: false });
+      undefinedPredicateWarning("Undefined", undefined);
+      undefinedPredicateWarning("Null", null);
+      done();
+    });
+  });
+});
